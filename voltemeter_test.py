11f23e11e79803ba9777fa9ab6d7d73c4b55ed63
Daniil Bykh <109330484+DaniilRen@users.noreply.github.com>
2023-09-23 16:29:12 +0300

    3

--- voltemeter_test.py
+++ voltemeter_test.py
@@ -18,6 +18,8 @@ def main(img):
     gray = cv.cvtColor(cropped, cv.COLOR_BGR2GRAY)
     gray = cv.inRange(gray, 120, 145)
     gray = cv.erode(gray, np.ones((3, 3), np.uint8))
+    cv.imshow('res', gray)
+    cv.waitKey(0)
 
     _, contours, __ = cv.findContours(gray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     for c in contours:
@@ -28,17 +30,28 @@ def main(img):
         box = cv.boxPoints(rect)
         box = np.int64(box)
         M = cv.moments(c)
-        cX = int(M['m10'] / M['m00']) # координаты центра
+        cX = int(M['m10'] / M['m00'])
         cY = int(M['m01'] / M['m00'])
-        x, y = box[0] # координаты конца стрелки
+        x, y = box[0]
 
         if x < gray.shape[1]/2: # если стрелка в левой части картинки
             cX -= 6 # имперически подобрал
 
+        # ang = 180*np.arctan2(x-cX, y-cY)/np.pi
         ang = (np.arctan2(-y + cY, x - cX) * (180 / np.pi)) % 360
 
-    print(round_((15 * ang / 360), 0.5))
+        # print(f'rect cx cy: {cx}, {cy}')
+        # print(f'Moments cx cy: {cX}, {cY}')
+        # print(f'-y + cY = {-y + cY}, x - cX = {x - cX}')
+        # print(f'-y + cY = {-y + cY}, x - cX = {x - cX}')
+        # print(f'rect - {rect}')
+        # print(f'box - {box}')
+        # print(f'counted angle - {ang}')
+        # print(f'new angle - {new_ang}')
+
+    print(15 * ang / 360)
 
 
 if __name__ == "__main__":
-    main(get_image(input()))
+    main(get_image('https://stepik.org/media/attachments/course/180710/volt_bac2b0686f.png'))
+    main(get_image('https://stepik.org/media/attachments/course/180710/volt_28523493f7.png'))
