481887f5fa55f492de1d51c21ef61cc16b887b91
Daniil Bykh <fffn12jk@gmail.com>
2023-11-03 18:48:53 +0300

    Update voltmeter.py

--- 1etap/inzhenerniy/voltmeter.py
+++ 1etap/inzhenerniy/voltmeter.py
@@ -11,46 +11,57 @@ def get_image(url):
     return image
 
 
-def round_(num, step): # округляет с шагом 0.5
+def round_(num, step):  # округляет с шагом 0.5
     return round(num / step) * step
 
+
+def coords_max_pixel(img):
+    x, y = img.shape
+    for i in range(x):
+        for j in range(y):
+            if img[i, j] == 255:
+                return i, j
+
+
+def coords_min_pixel(img):
+    x, y = img.shape
+    for j in range(y):
+        if img[x - 1, j] == 255:
+            return x - 1, j
+
+
+def tg_rad(a, b):
+    x = abs(a[0] - b[0])
+    y = b[1] - a[1]
+    if y > 0:
+        tg = x / y
+        return tg, False
+    tg = x / abs(y)
+    return tg, True
+
+
+def cretnost(doltan):
+    while doltan % 0.5 != 0:
+        doltan += 0.1
+    return doltan
+
+
 def main(img):
-    cropped = img[195:405, 165:495]
+    cropped = img[185:403, 115:565]
     gray = cv.cvtColor(cropped, cv.COLOR_BGR2GRAY)
     gray = cv.inRange(gray, 120, 145)
     gray = cv.erode(gray, np.ones((3, 3), np.uint8))
+    coords_a = coords_max_pixel(gray)
+    coords_b = coords_min_pixel(gray)
 
-    _, contours, __ = cv.findContours(gray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-    rect = cv.minAreaRect(contours[0])
-    box = cv.boxPoints(rect)
-    box = np.int64(box)
-    M = cv.moments(contours[0])
-    cX = int(M['m10'] / M['m00']) # координаты центра
-    cY = int(M['m01'] / M['m00'])
-    x, y = box[0] # координаты конца стрелки
-
-    if x < gray.shape[1]/2: # если стрелка в левой части картинки
-        cX -= 20 # имперически подобрал
-
-    end = [0, 0]
-    start = [1000, 1000]
-    for p in contours[0]:
-        p = p[0]
-        if p[0] < start[0]:
-            start = p
-        if p[0] > end[0]:
-            end = p
-
-    ang1 = (np.arctan2(-y + cY, x - cX) * (180 / np.pi)) % 90
-    ang2 = math.degrees(math.atan2(start[1] - end[1], start[0] - end[0]))
-
-    print(ang1, ang2)
-    print(round_((15 * ang1 / 360), 0.5))
-    print(round_((15 * ang2 / 360), 0.5))
-    print(15 * ang1 / 360)
-    print(15 * ang2 / 360)
+    tg_ang, flag = tg_rad(coords_a, coords_b)
+    ang = math.atan(tg_ang) * 180 / math.pi - 46.5
+    if flag:
+        result = 15 - cretnost(round(ang * 15 / 90, 1))
+    else:
+        result = cretnost(round(ang * 15 / 90, 1))
+    return result
 
 
 if __name__ == "__main__":
-    main(get_image(input()))
+    print(main(get_image(input())))
